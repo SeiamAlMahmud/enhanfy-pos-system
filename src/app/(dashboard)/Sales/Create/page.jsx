4a416d8c8f358fc80331e865862532f4f8a90bd1
SeiamAlMahmud <samahmud49@gmail.com>
2025-03-02 21:04:51 +0600

    update product fetch URL to use environment variable

--- src/app/(dashboard)/Sales/Create/page.jsx
+++ src/app/(dashboard)/Sales/Create/page.jsx
@@ -137,7 +137,8 @@ export default function AddSale() {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await fetch('/Products/Create/products');
+        // const response = await fetch('/Products/Create/products');
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products/get-products`);
         if (!response.ok) throw new Error('Failed to fetch products');
         const data = await response.json();
         setAvailableProducts(data);
